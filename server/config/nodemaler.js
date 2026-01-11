e92fc5058f321729ed45636d522b6472ac5a8644
PRATIK-RAKTATE <rhutikraktate@gmail.com>
2026-01-11 21:10:59 +0530

    fix: remove /smtp endpoint add nodemailer logging

--- server/config/nodemaler.js
+++ server/config/nodemaler.js
@@ -1,13 +1,27 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-const transport = nodemailer.createTransport({
-    host: 'smtp-relay.brevo.com',
-    port: 587,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    }
-
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  logger: true,
+  debug: true,
 });
 
-export default transport;
+export default transporter;
+
+export const verifySMTP = async () => {
+  try {
+    await transporter.verify();
+    console.log("✅ SMTP verified");
+  } catch (err) {
+    console.error("❌ SMTP verification failed", {
+      message: err.message,
+      code: err.code,
+    });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ import userRoutes from './routes/user.route.js';
 import healthRoutes from './health/health.routes.js';
 import metricsRoutes from './metrics/metrics.routes.js';
 
+import transporter, { verifySMTP } from "../server/config/nodemaler.js";
+
+
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -39,26 +42,7 @@ app.use('/metrics', metricsRoutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/user', userRoutes);
 
-app.get('/smtp', async (req, res) => {
-  try {
-    // Attempt to verify the connection
-    await transporter.verify();
-    
-    // If successful, send a 200 OK response
-    res.status(200).json({
-      status: "success",
-      message: "Brevo SMTP is connected and ready to send emails."
-    });
-  } catch (err) {
-    // If it fails, send a 500 Error response
-    res.status(500).json({
-      status: "error",
-      message: "SMTP verification failed",
-      error: err.message,
-      code: err.code
-    });
-  }
-});
+verifySMTP();
 
 // --- Start ---
 app.listen(port, () => {
