b9f806868b4926d2919861df31f985c1063b9aba
PRATIK-RAKTATE <rhutikraktate@gmail.com>
2026-01-25 22:27:39 +0530

    fix(remove): nodemailer import cause crashing server

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,13 +1,8 @@
 import { Router } from "express";
 import { 
-    isAuthenticated, 
     login, 
     logout, 
-    register, 
-    resetPassword, 
-    sendResetOtp, 
-    sendVerifyOtp, 
-    verifyEmail 
+    register,
 } from "../controllers/auth.controller.js";
 import userAuth from "../middlewares/auth.middleware.js";
 
@@ -16,10 +11,12 @@ const authRoutes = Router();
 authRoutes.post('/register', register);
 authRoutes.post('/login', login);
 authRoutes.post('/logout', logout);
+/*
 authRoutes.post('/send-verify-otp', userAuth, sendVerifyOtp);
 authRoutes.post('/verify-account', userAuth, verifyEmail);
 authRoutes.post('/is-auth', userAuth, isAuthenticated);
 authRoutes.post('/send-reset-otp', sendResetOtp);
 authRoutes.post('/reset-password', resetPassword);
+*/
 
 export default authRoutes;
